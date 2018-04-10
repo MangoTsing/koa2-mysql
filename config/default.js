b89d2b0f5978378287c5759139da461032a46c74
MangoTsing <757271842@qq.com>
2018-04-10 22:49:28 +0800

    18.4.10

---
+++ config/default.js
@@ -0,0 +1,14 @@
+const config = {
+  // 启动端口
+  port: 80,
+  // 数据库配置
+  database: {
+    DATABASE: 'nodesql',
+    USERNAME: 'root',
+    PASSWORD: 'm3g1997',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
